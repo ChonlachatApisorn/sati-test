9ab33f797c03a896136d769321990376e15f82b8
ChonlachatApisorn <105207643+ChonlachatApisorn@users.noreply.github.com>
2023-05-09 23:45:52 +0700

    fix: img tag profile (#17)

--- packages/client/src/app/components/profile.tsx
+++ packages/client/src/app/components/profile.tsx
@@ -19,29 +19,29 @@ export function ProfilePage() {
   return (
     <div className="flex justify-center bg-sky-950 w-screen h-screen">
       <div className="flex flex-col items-center bg-sky-900 font-bold text-sky-100 rounded-xl w-1/2 h-1/2 m-20 ">
-        {data.profile_image !== '' ? (
+        {data.profile_image === '' ? (
           <div className="bg-sky-100 rounded-full w-48 h-48 m-10" />
         ) : (
           <img
             src={data.profile_image}
             alt="profile_image"
-            className="rounded-full w-48 h-48 m-10"
+            className="rounded-full w-48 h-48 m-10 object-cover"
           />
         )}
         {/* profile image */}
-        <div className="flex items-start w-64 m-2">
+        <div className="flex items-start w-72 m-2">
           <span className="mx-3">First Name : </span>
           <span>{data.first_name}</span>
         </div>
-        <div className="flex items-start w-64 m-2">
+        <div className="flex items-start w-72 m-2">
           <span className="mx-3">Last Name : </span>
           <span>{data.last_name}</span>
         </div>
-        <div className="flex items-start w-64 m-2">
+        <div className="flex items-start w-72 m-2">
           <span className="mx-3">Email : </span>
           <span>{data.email}</span>
         </div>
-        <div className="flex items-start w-64 m-2">
+        <div className="flex items-start w-72 m-2">
           <span className="mx-3">Birthday : </span>
           <span>{data.date_of_birth}</span>
         </div>
